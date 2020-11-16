9b7eafe6e6e3c213f1c009400bf0d4e58f5d86c9
AnatolIgnatiev <61428823+AnatolIgnatiev@users.noreply.github.com>
2020-11-16 17:06:33 +0200

    Task 5 done

---
+++ Task 5/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    //nA, n — количество повторений символа (целое число от 2 до 99), 
+    //а A — заглавная английская буква, либо конструкции вида A, от 1 до 80 символов.
+    //трока должна быть разбита на строчки длиной ровно по 40 символов (за исключением последней, которая может содержать меньше 40 символов).
+    //	3A4B7D
+    // 22D7AC18FGD
+    class CodedString
+    {
+        public int NumberOfRepeats { get; set; }
+        public string Symbol { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputString = Console.ReadLine();
+            string result = "";
+            string numberOfRepeats = "";
+            Regex regex = new Regex("[1-99]");
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (regex.IsMatch(inputString[i].ToString()))
+                {
+                    numberOfRepeats += inputString[i];
+                }
+                else if (!string.IsNullOrEmpty(numberOfRepeats))
+                {
+                    for (int n = 0; n <= int.Parse(numberOfRepeats) -1; n++)
+                    {
+                        result += inputString[i];
+                    }
+                    numberOfRepeats = "";
+                }
+                else
+                {
+                    result += inputString[i];
+                }
+            }
+            var iterator = 0;
+            foreach (var item in result)
+            {
+                if (iterator <= 40)
+                {
+                    Console.Write(item);
+                    iterator++;
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                    iterator = 0;
+                }
+            }
+        }
+    }
+}
